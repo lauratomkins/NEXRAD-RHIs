4766a677da1376c66d95ca9f956f46c358a8ea39
lauratomkins <lmtomkin@ncsu.edu>
2022-06-23 14:50:51 -0400

    Update rhi_fun.py

--- utils/rhi_fun.py
+++ utils/rhi_fun.py
@@ -1,13 +1,13 @@
 import pyart
 from utils import rad_fun
 
-def constructRHI(radar, azimuth, field):
+def constructRHI(radar, azimuth, field, range=200000):
 
     # Find tilts with data for given field
     all_tilts, all_angs = rad_fun.returnAll(radar, field)
 
     # Due to the scan strategy, we have to remove the duplicate tilts
-    good_tilts = rad_fun.removeDuplicate(all_angs, all_tilts)
+    good_tilts, good_angs = rad_fun.removeDuplicate(all_angs, all_tilts)
     radar = radar.extract_sweeps(good_tilts)
 
     # generate cross section (RHI)
